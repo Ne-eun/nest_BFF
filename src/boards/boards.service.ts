6d4f68eedecbafaca986eb6d93a06ca9a3935dc9
ne-eun <p.tyche92@gmail.com>
2022-10-16 21:01:30 +0900

    feat: 게시물 로컬데이터에 저장하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 
 @Injectable()
 export class BoardsService {
   private boards: Array<Board> = [];
 
   getAllBoards(): Array<Board> {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const boardData = {
+      id: this.boards.length,
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(boardData);
     return this.boards;
   }
 }
