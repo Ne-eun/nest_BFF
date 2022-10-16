b0115e3978c33e3a376f8e81ef03ba2c978e87cc
ne-eun <p.tyche92@gmail.com>
2022-10-16 20:33:57 +0900

    feat: 로컬 게시글 데이터 모두 가져오기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-
+import { BoardsModule } from './boards/boards.module';
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  imports: [BoardsModule],
 })
 export class AppModule {}
